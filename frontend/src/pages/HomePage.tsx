f100510d39bd4a4d5f0da3cd27d7dcfebc60cac7
Kacper Stasik <stasikkacper1@gmail.com>
2025-05-09 13:39:55 -0500

    chore: boilerplate for react router

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,6 @@
+export default function HomePage() {
+  return (
+    <div className="flex min-h-screen flex-col">
+    </div>
+  )
+}
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+      <div className="flex min-h-screen flex-col">
+      </div>
+    )
+  }
+  
